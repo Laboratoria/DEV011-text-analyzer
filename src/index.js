d00b796a941960822aceddc1bcefe33686d470fc
GiselleLop <giselleLop@users.noreply.github.com>
2023-08-18 17:09:10 -0500

    Funcion del boton y estilos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+localStorage["text"] = document.querySelector('[name="user-input"]').value;  
+  
+
+const boton=document.querySelector("#reset-button");
+boton.addEventListener("click", limpiar)
+
+function limpiar() {
+    document.querySelector('[name="user-input"]').value="";
+}
+
