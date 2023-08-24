195b558a1f19c4c5d53f2fe81b67b518ce7dd59a
greysmpich <greys.mpich@gmail.com>
2023-08-24 09:45:49 -0600

    Se agregó la funcionalidad del conteo de caracteres

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,28 @@
-const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+const analyzer = {
+    getWordCount: (text) => {
+
+        //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    },
+    getCharacterCount: (text) => {
+        return text.length;
+        //const words = text.split(' ');
+
+        //text.value.length + ' characters';
+        //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+    },
+    getCharacterCountExcludingSpaces: (text) => {
+        //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    },
+    getAverageWordLength: (text) => {
+        //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    },
+    getNumberCount: (text) => {
+        //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    },
+    getNumberSum: (text) => {
+        //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    },
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import analyzer from './analyzer.js';
 
+const mensaje = document.querySelector("textarea");
+const contador = document.getElementById("contador");
+
+mensaje.addEventListener("keyup", function(e) {
+    const text = e.target.value;
+    const contCarAct = analyzer.getCharacterCount(text);
+    //const resultado= target.value.contCarAct;
+
+    contador.innerHTML = `Caracteres: ${contCarAct}`;
+});
+
+
+
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
