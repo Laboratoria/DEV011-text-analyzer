da20747ea5f8a069b215a7754fff10737bb64803
Paulibyus <paulina.gonzalezx@gmail.com>
2023-08-24 19:36:12 -0400

    experimeto1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textarea = document.getElementById("textArea");
+const limpiarBoton = document.getElementById("reset-button");
+limpiarBoton.addEventListener("click", function() {
+    return textarea.value = ""; // Establecer el valor del textarea como cadena vacía
+});
