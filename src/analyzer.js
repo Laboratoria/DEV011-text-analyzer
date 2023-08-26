a4db55d8d5ade78f77c0a4a9430b595d57f69af0
greysmpich <greys.mpich@gmail.com>
2023-08-26 09:45:13 -0600

    Se le dio funcionalidad al contador de números. Se le dio formato de estilos al contador de números con.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,26 +1,30 @@
 const analyzer = {
-    getWordCount: (text) => {
-        const words = text.split(" ");
-        return words.length;
-        //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    },
-    getCharacterCount: (text) => {
-        return text.length;
-        //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-
-    },
-    getCharacterCountExcludingSpaces: (text) => {
-        //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    },
-    getAverageWordLength: (text) => {
-        //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    },
-    getNumberCount: (text) => {
-        //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    },
-    getNumberSum: (text) => {
-        //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    },
+  getWordCount: (text) => {
+    const words = text.split(" ");
+    return words.length;
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getCharacterCount: (text) => {
+    return text.length;
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    const wordsNoSpaces = text.trim();
+    return wordsNoSpaces.length;
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getAverageWordLength: (text) => {
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getNumberCount: (text) => {
+    const str = text;
+    const numero = str.match(/\d/g);
+    return numero.length;
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getNumberSum: (text) => {
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  },
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 const mensaje = document.querySelector("textarea");
 const contadorCaract = document.querySelector(".caractCuenta");
 const contadorPalabras = document.querySelector(".palabrasCuenta");
+const contadorNumeros = document.querySelector(".numCuenta");
 
-mensaje.addEventListener("keyup", function(e) {
-    const text = e.target.value;
-    const contActCaract = analyzer.getCharacterCount(text);
-    contadorCaract.innerHTML = `Caracteres: ${contActCaract}`;
+mensaje.addEventListener("keyup", function (e) {
+  const text = e.target.value;
+  const contActCaract = analyzer.getCharacterCount(text);
+  contadorCaract.innerHTML = `Caracteres: ${contActCaract}`;
 });
 
-mensaje.addEventListener("keyup", function(e) {
-    const text = e.target.value;
-    const contActPalabras = analyzer.getWordCount(text);
-    contadorPalabras.innerHTML = `Palabras: ${contActPalabras}`;
+mensaje.addEventListener("keyup", function (e) {
+  const text = e.target.value;
+  const contActPalabras = analyzer.getWordCount(text);
+  contadorPalabras.innerHTML = `Palabras: ${contActPalabras}`;
+});
+
+mensaje.addEventListener("keyup", function (e) {
+  const text = e.target.value;
+  const contActNumeros = analyzer.getNumberCount(text);
+  contadorNumeros.innerHTML = `Números: ${contActNumeros}`;
 });
 //mensaje.addEventListener("focus", function(e) {
 //e.target.style.background = "#FF9966";
@@ -21,12 +28,10 @@ mensaje.addEventListener("keyup", function(e) {
 
 const btn = document.getElementById("resetButton");
 
-btn.addEventListener("click", function() { mensaje.value = " ", contadorCaract.innerHTML = `Caracteres: 0`, contadorPalabras.innerHTML = `Palabras: 0` });
-
-
-
-
-
-
+btn.addEventListener("click", function () {
+  (mensaje.value = " "),
+    (contadorCaract.innerHTML = `Caracteres: 0`),
+    (contadorPalabras.innerHTML = `Palabras: 0`);
+});
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
