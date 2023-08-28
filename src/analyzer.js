985d1939895a5888cba29e0e9a0ece2cf3c1b8dd
Elizabeth <chabelina2003@gmail.com>
2023-08-28 10:59:26 -0500

    Implementacion del metodo/funcionalidad getCharacterCount

--- src/analyzer.js
+++ src/analyzer.js
@@ -3,7 +3,7 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//Obtenemos el contenido que sea ingresado al textarea
+const textbox = document.querySelector('[name="user-input"]');
+//Obtenemos los elementos <li> y su valor
+const contadorMetrica = document.getElementsByClassName("metrica");
+
+//Empezamos a monitorear eventos en el DOM
+//Monitoreamos el ingreso de characteres en tiempo real del textarea.
+textbox.addEventListener("keyup",actualizarMetricas);
+
+//Funcion que actualiza las metricas tras un evento en tiempo real.
+function actualizarMetricas(){
+  //cuenta de characteres
+  //obtenemos la metrica de
+  const CharCount = analyzer.getCharacterCount(textbox.value);
+  contadorMetrica[0].textContent = "Caracteres " + CharCount;
+}
+
