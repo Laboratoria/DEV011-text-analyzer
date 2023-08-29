1a358144b4d845c83712a3bc48861ec76c40baaf
Elizabeth <chabelina2003@gmail.com>
2023-08-29 14:30:54 -0500

    Implementacion del metodo/funcionalidad getCharacterCountExcludingSpaces

--- src/analyzer.js
+++ src/analyzer.js
@@ -7,6 +7,12 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //numero de espacios
+    const numEspacios = text.split(" ").length - 1;
+    if(numEspacios > 0){
+      return text.length - numEspacios;
+    }
+    else return text.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import analyzer from './analyzer.js';
 //Obtenemos el contenido que sea ingresado al textarea
 const textbox = document.querySelector('[name="user-input"]');
 //Obtenemos los elementos <li> y su valor
-const contadorMetrica = document.getElementsByClassName("metrica");
+const contadoresMetrica = document.getElementsByClassName("metrica");
 
 //Empezamos a monitorear eventos en el DOM
 //Monitoreamos el ingreso de characteres en tiempo real del textarea.
@@ -12,8 +12,13 @@ textbox.addEventListener("keyup",actualizarMetricas);
 //Funcion que actualiza las metricas tras la deteccion de un evento en tiempo real.
 function actualizarMetricas(){
   //cuenta de characteres
-  //obtenemos la metrica de
+  //obtenemos la metrica de caracteres con uso del objeto analyzer
   const CharCount = analyzer.getCharacterCount(textbox.value);
-  contadorMetrica[0].textContent = "Caracteres: " + CharCount;
+  contadoresMetrica[0].textContent = "Caracteres: " + CharCount;
+  //obtenemos la metrica de caracteres con uso del objeto analyzer
+  const CharCountExSpace = analyzer.getCharacterCountExcludingSpaces(textbox.value);
+  contadoresMetrica[1].textContent = "Caracteres sin espacios: " + CharCountExSpace;
+
+  
 }
 
