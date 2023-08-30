89771adad154f01bbe06fc030aafb59b5d082c62
Elizabeth <chabelina2003@gmail.com>
2023-08-30 11:35:57 -0500

    Implementacion de la funcionalidad del boton para Limpiar Texto

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import analyzer from './analyzer.js';
 const textbox = document.querySelector('[name="user-input"]');
 //Obtenemos los elementos <li> y su valor
 const contadoresMetrica = document.getElementsByClassName("metrica");
+const botonReset = document.getElementById("reset-button");
 
 //Empezamos a monitorear eventos en el DOM
 //Monitoreamos el ingreso de characteres en tiempo real del textarea.
 textbox.addEventListener("keyup",actualizarMetricas);
+botonReset.addEventListener("click", limpiarTexto)
 
 //Funcion que actualiza las metricas tras la deteccion de un evento en tiempo real.
 function actualizarMetricas(){
@@ -17,8 +19,11 @@ function actualizarMetricas(){
   contadoresMetrica[0].textContent = "Caracteres: " + CharCount;
   //obtenemos la metrica de caracteres con uso del objeto analyzer
   const CharCountExSpace = analyzer.getCharacterCountExcludingSpaces(textbox.value);
-  contadoresMetrica[1].textContent = "Caracteres sin espacios: " + CharCountExSpace;
+  contadoresMetrica[1].textContent = "Caracteres Sin Espacios: " + CharCountExSpace;
+}
 
-  
+function limpiarTexto(){
+  textbox.value = "";
+  actualizarMetricas();
 }
 
