200d2cbbf438b12e39547dc50c9e57a5a1397e4e
Tiare Infante <tiare.infante.snmartin@gmail.com>
2023-08-29 20:28:40 -0400

    agregar 3 metodos en analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,12 +1,14 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(' ');
+    return words.length
   },
+  
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text.replace( /[,.\s]/g, '' ).length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const input = document.querySelector('[name="user-input"]');
+const wordCountList = document.querySelector('[data-testid="word-count"]');
+const resetButton = document.getElementById ('reset-button');
+const characterCountList = document.querySelector('[data-testid="character-count"]')
+const characterWithoutSpaceList = document.querySelector('[data-testid="character-no-spaces-count"]')
+
+input.addEventListener('keyup', () => {
+  const userInput = input.value;
+  if(userInput === ''){
+    wordCountList.textContent = 'Conteo de Palabras: 0';
+    characterCountList.textContent = 'Caracteres: 0';
+    characterWithoutSpaceList.textContent = 'Caracteres sin Espacios: 0';
+
+  } else {
+    const wordCount = analyzer.getWordCount(userInput);
+    wordCountList.textContent = `Conteo de Palabras: ${wordCount}`;
+    const characterCount = analyzer.getCharacterCount(userInput);
+    characterCountList.textContent = `Caracteres: ${characterCount}`;
+    const characterWithoutSpaceCount = analyzer.getCharacterCountExcludingSpaces(userInput);
+    characterWithoutSpaceList.textContent = `Caracteres sin Espacios: ${characterWithoutSpaceCount}`;
+  }
+  
+});
+
+resetButton.addEventListener ('click', () => {
+  input.value = '';
+  wordCountList.textContent = 'Conteo de Palabras: 0';
+  characterCountList.textContent = 'Caracteres: 0';
+  characterWithoutSpaceList.textContent = 'Caracteres sin Espacios: 0';
+
+});
