7703a9e454287d5b7b79f182d336c04d10d37e1c
GiselleLop <giselleLop@users.noreply.github.com>
2023-08-31 15:06:41 -0500

    Arreglo

--- src/index.js
+++ src/index.js
@@ -4,52 +4,52 @@ import analyzer from './analyzer.js';
 
 // Campo de entrada de texto
 const inputTexto = document.querySelector('[name="user-input"]');
-inputTexto.addEventListener("input", contadorPalabras);
+inputTexto.addEventListener("keyup", contadorPalabras);
 
 //CONTADOR DE PALABRAS
 function contadorPalabras() {
   const wordcount = document.querySelector('[data-testid="word-count"]');
-  wordcount.textContent=analyzer.getWordCount(inputTexto.value)
+  wordcount.textContent="Palabras: " + analyzer.getWordCount(inputTexto.value)
   contadorCaracterEsp()
 }
 
 //CONTADOR DE CARACTERES INCLUYENDO ESPACIOS
 function contadorCaracterEsp() {
   const Carcount = document.querySelector('[data-testid="character-count"]');
-  Carcount.textContent=analyzer.getCharacterCount(inputTexto.value)
+  Carcount.textContent="Caracteres: " + analyzer.getCharacterCount(inputTexto.value)
   contadorCaractersinespacio()
 }
 
 //CONTADOR DE CARACTERES SIN ESPACIOS NI SIGNOS DE PUNTUACION
 function contadorCaractersinespacio() {
   const wordcountsig = document.querySelector('[data-testid="character-no-spaces-count"]');
-  wordcountsig.textContent=analyzer.getCharacterCountExcludingSpaces(inputTexto.value)
+  wordcountsig.textContent="Caracteres sin espacio: "+ analyzer.getCharacterCountExcludingSpaces(inputTexto.value)
   contadorNum()
 }
 
 //CONTADOR DE NUMEROS
 function contadorNum() {
   const numcount = document.querySelector('[data-testid="number-count"]');
-  numcount.textContent=analyzer.getNumberCount(inputTexto.value)
+  numcount.textContent="Números: " + analyzer.getNumberCount(inputTexto.value)
   sumaNum()
 }
     
 //SUMA DE NUMEROS  
 function sumaNum() {
   const numsum = document.querySelector('[data-testid="number-sum"]');
-  numsum.textContent=analyzer.getNumberSum(inputTexto.value)
+  numsum.textContent="Suma de números: "+ analyzer.getNumberSum(inputTexto.value)
   promedio()
 }   
 
 //PROMEDIO
 function promedio() {
   const promedio = document.querySelector('[data-testid="word-length-average"]');
-  promedio.textContent=analyzer.getAverageWordLength(inputTexto.value)
+  promedio.textContent="Promedio longitud de palabras: " + analyzer.getAverageWordLength(inputTexto.value)
 }
 
 //Boton  limpiar metricas
 
-const boton=document.querySelector("#reset-button");
+const boton=document.getElementById("reset-button");
 boton.addEventListener("click", limpiar)
 
 function limpiar(){
@@ -61,11 +61,11 @@ function limpiar(){
   const numsum = document.querySelector('[data-testid="number-sum"]');
   const promedio = document.querySelector('[data-testid="word-length-average"]');
 
-  wordcount.textContent=0;
-  Carcount.textContent=0;
-  wordcountsig.textContent=0;
-  numcount.textContent=0;
-  numsum.textContent=0;
-  promedio.textContent=0;
+  wordcount.textContent="Palabras: " + 0;
+  Carcount.textContent="Caracteres: " + 0;
+  wordcountsig.textContent="Caracteres sin espacios: "+ 0;
+  numcount.textContent="Números: " + 0;
+  numsum.textContent="Suma de números: " + 0;
+  promedio.textContent="Promedio longitud de palabras: " + 0;
 
 }
