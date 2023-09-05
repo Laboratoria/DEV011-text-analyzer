56d95b45d5dc44a2eefa60fd106a8b6903c36ef8
Paulibyus <paulina.gonzalezx@gmail.com>
2023-09-04 23:33:38 -0300

    avancesignificativo

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,28 +1,43 @@
-const analyzer = {  
-  getWordCount: (text) => { 
-   const textoAreaDividido = text.split(" ")
-  return textoAreaDividido.length
+const analyzer = {
+  getWordCount: (text) => {
+    const textoAreaDividido = text.trim().split(" ");
+    return textoAreaDividido.length;
+
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
-    return text.length
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    const caracteresSinEspacios = text.replace(/\s+/g, " ")
-    return caracteresSinEspacios.length 
-    //caracteresSinEspacios.length
+    const caracteresSinEspacios = text.trim().replace(/\s+/g, ""); //expresión regular que se utiliza para buscar y reemplazar espacios en blanco en una cadena de texto.//
+    return caracteresSinEspacios.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text;
   },
   getNumberCount: (text) => {
+    let onlynumbers = text.match(/\d+/g); // secuencias de uno o más dígitos en una cadena de texto//
+    if (onlynumbers === null) {
+      onlynumbers = "";
+    }
+    return onlynumbers.length;
     // return NumberCount
+
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const listaText = text.replace(/\s+/g, ' ').trim().split('');
+    let sumNumber = 0;
+    for (let i = 0; i < listaText.length; i++){
+      if (!isNaN(listaText[i]/1)){
+        sumNumber += Number(listaText[i]);
+      }
+    }
+    return sumNumber;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-let textArea = document.getElementsByName("user-input")[0]; /*manipulamos el DoM a través de document.*/
-let wordCounter = document.getElementsByClassName("word-counter")[0];
-let characterCount = document.getElementsByClassName ("character-count")[0];
+const textArea =
+  document.getElementsByName(
+    "user-input"
+  )[0]; /*manipulamos el DoM a través de document.*/
+const wordCounter = document.getElementsByClassName("word-counter")[0];
+const characterCount = document.getElementsByClassName("character-count")[0];
 const limpiarBoton = document.getElementById("reset-button");
 limpiarBoton.addEventListener("click", function () {
-    textArea.value = ""; // Establecer el valor del textarea como cadena vacía
+  textArea.value = ""; // Establecer el valor del textarea como cadena vacía
 });
 
-textArea.addEventListener("keyup", function() {
-    let textValue = textArea.value
-    wordCounter.textContent = analyzer.getWordCount(textValue)                               
-    characterCount.textContent = analyzer.getCharacterCount(textValue)
-    CharacterCountExcludingSpaces.textContent = analyzer.getCharacterCountExcludingSpaces(textValue)
-})
+textArea.addEventListener("keyup", function () {
+  const textValue = textArea.value;
+  wordCounter.textContent = analyzer.getWordCount(textValue);
+  characterCount.textContent = analyzer.getCharacterCount(textValue);
+});
+
+const CharacterCountExcludingSpaces = document.querySelector("li[data-testid='character-no-spaces-count']");
+textArea.addEventListener("keyup", function () {
+  const textValue = textArea.value;
+  CharacterCountExcludingSpaces.innerHTML = 'Recuento de caracteres excluyendo espacios y signos de puntuación  <p class="character-no-spaces-count">'+ analyzer.getCharacterCountExcludingSpaces(textValue) + '</p>'
+    
+});
+
+const NumberCount = document.getElementsByClassName("number-count")[0];
+textArea.addEventListener("keyup", function () {
+  const textValue = textArea.value;
+  NumberCount.textContent = analyzer.getNumberCount(textValue);
+});          
 
 
-let CharacterCountExcludingSpaces = document.getElementsByClassName ("character-no-spaces-count1")[0];
-textArea.addEventListener("keyup", function(){
-    const total2 = analyzer.getCharacterCountExcludingSpaces (textArea.value)
-    // console.log (total2)
-    return CharacterCountExcludingSpaces
-})
-
-textArea.addEventListener ("keyup", function() {    /*keyup es la acción de addEventListener escucha, en este caso, el texto que se ingresa en el textarea*/
-    const total3 = analyzer.getNumberCount (textArea.value)
-    console.log (total3)
-})
-
-let NumberCount = document.getNumberCount ("number-count")
-textArea.addEventListener ("keyup", function(){ 
-    const total3 = analyzer.getNumberCount (textArea.value)
-    NumberCount.textContent = total3
-    return NumberCount
-})
+const NumberSum = document.getElementsByClassName("number-sum")[0];
+textArea.addEventListener("keyup", function () {
+  const textValue = textArea.value;
+  NumberSum.textContent = analyzer.getNumberSum(textValue);
+});
