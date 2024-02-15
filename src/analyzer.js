430615e62b63fbc0e51dc714e2ff2d79de886bfd
GiselleLop <giselleLop@users.noreply.github.com>
2024-02-15 18:17:27 -0500

    fix(components): refactorizacion de codigo, arreglos en los estilos

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,58 +1,27 @@
 const analyzer = {  
   getWordCount: (text) => {
-  /*
-  En ésta función se define una variable llamada vari; la cual contiene un regex que selecciona todos los espacios y saltos de línea
-  Luego se define una variable llamada palabras, en la que se utiliza la función replace() para eliminar los caracteres de puntuación y números del texto, utilizando otro regex. 
-  Después, se utiliza trim() para eliminar los espacios en blanco al principio y al final del texto.
-  Finalmente, se utiliza split() con el regex puesto en la variable vari, para dividir el texto en palabras.
-  Luego utilizamos una condicional if, que nos dice que si la longitudo de la variable palabras es mayor a 0, se retorna la longitud del array, lo cual seria el numero de palabras.
-  */
-    const vari = /[\s\n]+/g
-    const palabras = text.replace(/[.,;?¿!¡{}:'"/-1234567890]/gi,"").trim().split(vari); 
-    if (palabras.length>0){
-      return palabras.length
-    }
+    if (text==="") {
+      return 0
+    } 
+    const regexWord = /[\s\n]+/g
+    const words = text.replace(/[.,;?¿!¡{}:'"/-1234567890]/gi,"").trim().split(regexWord);
+    return words.length
   },
   getCharacterCount: (text) => {
-    /*
-    Se define una variable llamada caracter, en la que se utiliza el metodo split("") para separar cada caracter del texto ingresado.
-    Luego usamos la condiciona if; que nos dice que si el primer caracter de la variable "caracter" esta vacio, retorne 0.
-    Si esta condicion no se cumple, se retorna la longitud del array qque creamos.
-    */
-    const caracter = text.split("");
-    if (caracter[0]==="") {
+    if (text==="") {
       return 0
-    }
-    else { 
-      return caracter.length
     }  
+    const character = text.split("");
+    return character.length
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //Esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    /*
-    En este código, se define una variable llamada caraSig, en la que se utiliza el método replace() para eliminar los espacios y signos de puntuación del texto ingresado. 
-    Luego, se utiliza el método split() para convertir el texto en un array de caracteres. 
-    Si el primer elemento de ese array es vacío, la función retorna 0. 
-    De lo contrario, retorna la longitud del array de caracteres resultante, lo cual sería el recuento de caracteres excluyendo espacios y caracteres especiales.
-    */ 
-    const caraSig = text.replace(/[.,; ?¿!¡{}:'"-]/gi,"").split("");
-    if (caraSig[0]==="") {
+    if (text==="") {
       return 0
-    }
-    else { 
-      return caraSig.length
-    }
+    }  
+    const caraSig = text.replace(/[.,; ?¿!¡{}:'"-]/gi,"").split("");
+    return caraSig.length
   },
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    /*
-    Este código calcula la longitud promedio de las palabras en el texto. 
-    Primero, se define un regex de espacios en una variable llamada "espacios"
-    Luego, se inicializa una variable "sum" en 0. 
-    Se define una variable llamada arrPalabras, en la que el texto se recorta y se divide en un array de palabras utilizando el regex "espacios". 
-    Luego, se itera sobre cada palabra del array y se suma su longitud a la variable "sum". 
-    Finalmente, se calcula el promedio dividiendo la suma por la cantidad de palabras y se redondea a 2 decimales antes de retornarlo.
-    */
     const espacios = /[\s]+/
     let sum = 0
     const arrPalabras = text.trim().split(espacios)
@@ -67,48 +36,26 @@ const analyzer = {
  
   },
   getNumberCount: (text) => {
-    //Esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    /*
-    Este código cuenta cuántos números hay en el texto. 
-    Primero, se utiliza el método match junto con una expresión regular para encontrar todos los números en el texto. 
-    Luego, se verifica si la longitud del array resultante es mayor a cero. Si es así, se retorna esa longitud como resultado, es decir, la cantidad de números encontrados en el texto, es decir, la cantidad de numero que se encontraron. 
-    De lo contrario, se retorna cero.
-    */ 
     const num = text.match(/\b\d+(\.\d+)\b|\b\d+\b/g);
-    if(num.length >0) {
-      return num.length
+    if(num===null) {
+      return 0
     }
     else {
-      return 0
+      return num.length
     } 
   },
   
   getNumberSum: (text) => {
-    //Esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    /*
-    Este código calcula la suma de todos los números que se encuentran en el parámetro text.
-    Primero, se inicializa la variable suma en cero. 
-    Luego, se define la variable Number y se utiliza el método match junto con una expresión regular para encontrar todos los números en el texto. 
-    Después, se recorre cada elemento del array Number y se va sumando su valor a la variable suma. 
-    Finalmente, se retorna el valor de suma, que es la suma total de los números encontrados en el texto. 
-    Si no se encuentra ningún número, se retorna cero.
-    */
-   
     let suma = 0
-    const Number = text.match(/\b\d+(\.\d+)\b|\b\d+\b/g)
-   
-    for(let i=0; i<Number.length; i++) {
-      
-      if (Number.length===0){
-      return 0
-      }
-      else {
-      suma = suma + parseFloat(Number[i])
-      return suma
-      }
+    const numberArray = text.match(/\b\d+(\.\d+)\b|\b\d+\b/g)
+    if (numberArray===null){
+      return 0 
     }
-    
-  },
+    for(let i=0; i<numberArray.length; i++) {
+      suma = suma + parseFloat(numberArray[i])
+    }
+    return suma
 
+  },
 };
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,80 +1,64 @@
-import analyzer from './analyzer.js';
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+import analyzer from "./analyzer.js";
 
+const inputText = document.querySelector('[name="user-input"]');
+inputText.addEventListener("keyup", () => calculateMetrics(inputText.value));
 
-// Campo de entrada de texto 
-//En esta constante llamada inputTexto; se le aplica el método querySelector; que recupera el elemento de DOM del textarea
-//Luego, se le garega el evento Keyup, lo que significa que al momento de que se presione alguna tecla, va a ocurrir la funcion de contadorpalabaras
-const inputTexto = document.querySelector('[name="user-input"]');
-inputTexto.addEventListener("keyup", contadorPalabras);
-
-//CONTADOR DE PALABRAS
-//Esta funcion llamada contadorPalabras contiene una constante llamada wordcount, en esta constatnte, se le aplica el metodo queryselector, que recupera/selecciona el atributo datatestid,wordcount
-//Luego a la constante wordcount, se le agrega la propiedad textconten, que significa que en en donde se ha recuperado el atributo, se debe imprimir el contenido especificado
-function contadorPalabras() {
-  const wordcount = document.querySelector('[data-testid="word-count"]');
-  wordcount.textContent="Palabras: " + analyzer.getWordCount(inputTexto.value)
-  contadorCaracterEsp()
+const metrics = [
+  "Words",
+  "Characters",
+  "Characters without space",
+  "Numbers",
+  "Sum of numbers",
+  "Average word length",
+];
+const metricsContainer = document.querySelector(".metricsContainerPrincipal");
+for (const metric of metrics) {
+  const container = document.createElement("div");
+  const subTitle = document.createElement("h1");
+  container.classList.add("metric-container");
+  container.id = metric;
+  subTitle.textContent = metric + ": " + 0;
+  container.append(subTitle);
+  metricsContainer.appendChild(container);
 }
 
-//CONTADOR DE CARACTERES INCLUYENDO ESPACIOS
-function contadorCaracterEsp() {
-  const Carcount = document.querySelector('[data-testid="character-count"]');
-  Carcount.textContent="Caracteres: " + analyzer.getCharacterCount(inputTexto.value)
-  contadorCaractersinespacio()
+function calculateMetrics(text) {
+  const metricContainers = document.querySelectorAll(".metric-container");
+  metricContainers.forEach((container) => {
+    const metricId = container.id;
+    switch (metricId) {
+    case "Words":
+      container.querySelector("h1").textContent =
+          "Words: " + analyzer.getWordCount(text);
+      break;
+    case "Characters":
+      container.querySelector("h1").textContent =
+          "Characters: " + analyzer.getCharacterCount(text);
+      break;
+    case "Characters without space":
+      container.querySelector("h1").textContent =
+          "Characters without space: " +
+          analyzer.getCharacterCountExcludingSpaces(text);
+      break;
+    case "Numbers":
+      container.querySelector("h1").textContent =
+          "Numbers: " + analyzer.getNumberCount(text);
+      break;
+    case "Sum of numbers":
+      container.querySelector("h1").textContent =
+          "Sum of numbers: " + analyzer.getNumberSum(text);
+      break;
+    case "Average word length":
+      container.querySelector("h1").textContent =
+          "Average word length: " + analyzer.getAverageWordLength(text);
+      break;
+    default:     
+      break;
+    }
+  });
 }
-
-//CONTADOR DE CARACTERES SIN ESPACIOS NI SIGNOS DE PUNTUACION
-function contadorCaractersinespacio() {
-  const wordcountsig = document.querySelector('[data-testid="character-no-spaces-count"]');
-  wordcountsig.textContent="Caracteres sin espacio: "+ analyzer.getCharacterCountExcludingSpaces(inputTexto.value)
-  contadorNum()
-}
-
-//CONTADOR DE NUMEROS
-function contadorNum() {
-  const numcount = document.querySelector('[data-testid="number-count"]');
-  numcount.textContent="Números: " + analyzer.getNumberCount(inputTexto.value)
-  sumaNum()
-}
-    
-//SUMA DE NUMEROS  
-function sumaNum() {
-  const numsum = document.querySelector('[data-testid="number-sum"]');
-  numsum.textContent="Suma de números: "+ analyzer.getNumberSum(inputTexto.value)
-  promedio()
-}   
-
-//PROMEDIO
-function promedio() {
-  const promedio = document.querySelector('[data-testid="word-length-average"]');
-  promedio.textContent="Promedio longitud de palabras: " + analyzer.getAverageWordLength(inputTexto.value)
-}
-
-//Boton  limpiar metricas
-/*
-Este código asigna un evento de clic al botón con el id "reset-button". 
-Cuando se hace clic en el botón, la función limpiar se ejecuta. 
-La función limpiar establece el valor del elemento inputTexto en una cadena vacía y actualiza los valores de varios elementos del DOM con los valores correspondientes a cero. 
-Estos elementos son seleccionados utilizando selectores de atributos y sus valores se actualizan utilizando la propiedad textContent.
-*/
-const boton=document.getElementById("reset-button");
-boton.addEventListener("click", limpiar)
-
-function limpiar(){
-  inputTexto.value="";
-  const wordcount = document.querySelector('[data-testid="word-count"]');
-  const Carcount = document.querySelector('[data-testid="character-count"]');
-  const wordcountsig = document.querySelector('[data-testid="character-no-spaces-count"]');
-  const numcount = document.querySelector('[data-testid="number-count"]');
-  const numsum = document.querySelector('[data-testid="number-sum"]');
-  const promedio = document.querySelector('[data-testid="word-length-average"]');
-
-  wordcount.textContent="Palabras: " + 0;
-  Carcount.textContent="Caracteres: " + 0;
-  wordcountsig.textContent="Caracteres sin espacios: "+ 0;
-  numcount.textContent="Números: " + 0;
-  numsum.textContent="Suma de números: " + 0;
-  promedio.textContent="Promedio longitud de palabras: " + 0;
-
-}
+const buttonReset = document.getElementById("reset-button");
+buttonReset.addEventListener("click", ()=> {
+  inputText.value = '';
+  calculateMetrics(inputText.value)
+});
